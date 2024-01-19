1703af602c7106a3c6e8ee8c8fb49d701ac6c70b
choitoady <healutoday@naver.com>
2024-01-19 15:07:46 +0900

    first commit

---
+++ 제네릭.ts
@@ -0,0 +1,34 @@
+// 제네릭 클래스 (큐 방식으로 동작하는 클래스)
+
+
+class GenericQueue<T> {
+    private items: T[] = [];
+
+    enqueue(items: T): void {
+        this.items.push(items);
+    }
+
+    dequeue(): T | undefined {
+        return this.items.shift()
+    }
+    peek(): T | undefined {
+        return this.items[0]
+    }
+    
+    size(): number {
+        return this.items.length;
+    }
+}
+
+const stringQ = new GenericQueue<string>();
+stringQ.enqueue("Hello");
+console.log(stringQ.peek());
+stringQ.dequeue();
+stringQ.enqueue("TypeScript");
+console.log(stringQ.size());
+console.log(stringQ.peek());
+
+
+const numberQ = new GenericQueue<number>();
+
+numberQ.enqueue(10)
